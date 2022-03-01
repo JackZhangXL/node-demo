de8da30fa52ec15a0d039bb15d649d50428ecf1e
zhangxinlin <zhangxinlin@meituan.com>
2022-03-01 11:30:57 +0800

    update

--- src/rpc/duplex/lib.js
+++ src/rpc/duplex/lib.js
@@ -1,29 +1,39 @@
 /**
- * 检查一段buffer是不是一个完整的数据包。
- * 具体逻辑是：判断header的bodyLength字段，看看这段buffer是不是长于header和body的总长
- * 如果是，意味着这个请求包是完整的，则返回这个包长。
- * 如果不是，则返回0，意味着包还没接收完
+ * 一个rpc的数据包会分为【定长的包头】和【不定长的包体】两部分
+ * 【定长的包头】用来记录序号seq和包长度bodyLen
  */
-function checkComplete(buffer) {
-    if (buffer.length < 6) {
-        return 0
-    }
-    const bodyLength = buffer.readInt32BE(2)
-    return 6 + bodyLength
+function encode(data) {
+    const { seq, body } = data;
+
+    const header = Buffer.alloc(6)
+    header.writeInt16BE(seq)
+    header.writeInt32BE(body.length, 2)
+
+    return Buffer.concat([header, body])    // 【定长的包头】和【不定长的包体】连起来
 }
 
 function decode(buffer) {
     const header = buffer.slice(0, 6)
-    const seq = header.readInt16BE()
-    const body = buffer.slice(6)
-
     return {
-        seq,
-        data: body
+        seq: header.readInt16BE(),
+        body: buffer.slice(6)
     }
 }
 
+/**
+ * 检查一段buffer是不是一个完整的数据包。
+ * 具体逻辑是：
+ * 1.buffer长度 <【定长的包头】长度，说明不完整
+ * 2.buffer长度 <【定长的包头】长度 +【定长的包头】里bodyLen长度，说明不完整
+ */
+ function checkComplete(buffer) {
+    if (buffer.length < 6) return 0
+    const len = 6 + buffer.readInt32BE(2)
+    return buffer.length < len ? 0 : len
+}
+
 module.exports = {
+    encode,
+    decode,
     checkComplete,
-    decode
 }
---
+++ src/rpc/duplex/serverB.js
@@ -0,0 +1,33 @@
+// http://nodejs.cn/api/net.html
+const net = require('net')
+const fs = require('fs')
+const { checkComplete, encode, decode } = require('./lib.js')
+const protobuf = require('protocol-buffers')
+const proto = protobuf(fs.readFileSync(`${__dirname}/demo.proto`))
+
+const server = net.createServer((socket) => {   // 创建tcp服务器
+    socket.on('data', (buffer) => {    
+        let packageLen = 0
+        while (packageLen = checkComplete(buffer)) {
+            const package = buffer.slice(0, packageLen)
+            console.log('package', package)
+            buffer = buffer.slice(packageLen)
+            const request = decode(package)
+            console.log('request', request)
+
+            const timeout = Math.floor(Math.random() * 1000)
+            console.log('处理耗时:', timeout)
+            setTimeout(() => {   // 模拟服务器处理业务一段时间后返回数据
+                socket.write(encode({ 
+                    seq: request.seq,
+                    body: proto.Person.encode({
+                        name: 'Sarah',
+                        id: 123
+                    })
+                }))
+            }, timeout)
+        }
+    })
+})
+
+server.listen(4000)
